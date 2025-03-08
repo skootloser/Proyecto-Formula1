2456f126692fd654b3c7ca20992429b2227c4837
skootloser <abrahamsotomoises@gmail.com>
2025-03-07 21:42:10 -0400

    Conexion de R a data SQLite Formula 1

--- Prueba Print.R
+++ Prueba Print.R
@@ -2,20 +2,14 @@
 library(readxl)
 library(dplyr)
 
-# Se incluyen varias datas de la data sqlite
+# Conexión con data SQLite
 
-file.choose() #Código para encontrar el archivo .xlsx de las datas
+library(RSQLite)
+library(DBI)
+ruta <- "C:/Users/Abraham/Desktop/UCV/SQL UCV/Formula 1 DATA/Formula1.sqlite"
+conexion <- dbConnect(RSQLite::SQLite(), ruta)
 
-circuits <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  1)
-constructor_results  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  2)
-constructor_standings  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  3)
-constructors  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  4)
-driver_standings  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  5)
-drivers  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  6)
-laptimes  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  7)
-pitstops  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  8)
-qualifying  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  9)
-races  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  10)
-results  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  11)
-seasons  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  12)
-status  <- read_excel("C:\\Users\\Abraham\\Desktop\\UCV\\SQL UCV\\Formula 1 Proyecto\\Formula 1 Data.xlsx" , sheet =  13)
+# Prueba de conexion generando la primera tabla
+
+corredores <- dbGetQuery(conexion, "SELECT * FROM drivers") 
+print("Datos de la tabla: drivers")
