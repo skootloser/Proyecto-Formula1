e65bbf4406d8ea0432555c491108e4b750470790
skootloser <abrahamsotomoises@gmail.com>
2025-03-16 18:26:46 -0400

    Creación de vista 5 Circuitos con mas Carreras

--- Formula 1 Main.R
+++ Formula 1 Main.R
@@ -1,5 +1,6 @@
 ## Los 5 circuitos con mas carreras ## 
-circuitos_carreras_5 <- "SELECT  
+circuitos_carreras_5 <- " CREATE VIEW circuitos_con_mascarreras 
+SELECT  
 a.circuitRef, 
 a.name, 
 a.location, 
@@ -8,6 +9,8 @@ COUNT(b.raceId) AS TotalCarreras
 FROM circuits a INNER JOIN races b ON a.circuitId = b.circuitId 
 GROUP BY a.circuitId 
 ORDER BY TotalCarreras DESC 
-LIMIT 5;"  ´
+LIMIT 5;"
 
-circuitos_con_mascarreras <- dbGetQuery(conexion, circuitos_carreras_5) #de esta forma obtenemos los 5 circuitps con mas carreras
+circuitos_con_mascarreras <- dbGetQuery(conexion, circuitos_carreras_5)
+
+## de esta forma obtenemos los 5 circuitoss con mas carreras ##
